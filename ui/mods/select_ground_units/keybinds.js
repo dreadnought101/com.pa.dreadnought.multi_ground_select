353f84f9f2b99d63cb18c3aefbfb01b3ac6f3415
dreadnought101 <michaeljonesrox@gmail.com>
2020-12-14 21:26:57 -0700

    Initial commit

---
+++ ui/mods/select_ground_units/keybinds.js
@@ -0,0 +1,39 @@
+action_sets.gameplay.select_ground_units_on_screen_1 = function () {
+  if (select_ground_units_on_screen_1) select_ground_units_on_screen_1()
+}
+
+action_sets.gameplay.select_ground_units_on_screen_2 = function () {
+  if (select_ground_units_on_screen_2) select_ground_units_on_screen_2()
+}
+
+action_sets.gameplay.select_ground_units_on_screen_3 = function () {
+  if (select_ground_units_on_screen_3) select_ground_units_on_screen_3()
+}
+
+
+api.settings.definitions.keyboard.settings.select_ground_units_on_screen_1 = {
+  title: 'Select Ground Units 1',
+  type: 'keybind',
+  set: 'mods',
+  display_group: 'mods',
+  display_sub_group: 'Select Ground Units',
+  default: 'alt+shift+1'
+}
+
+api.settings.definitions.keyboard.settings.select_ground_units_on_screen_2 = {
+  title: 'Select Ground Units 2',
+  type: 'keybind',
+  set: 'mods',
+  display_group: 'mods',
+  display_sub_group: 'Select Ground Units',
+  default: 'alt+shift+2'
+}
+
+api.settings.definitions.keyboard.settings.select_ground_units_on_screen_3 = {
+  title: 'Select Ground Units 3',
+  type: 'keybind',
+  set: 'mods',
+  display_group: 'mods',
+  display_sub_group: 'Select Ground Units',
+  default: 'alt+shift+3'
+}
---
+++ ui/mods/select_ground_units/live_game.js
@@ -0,0 +1,21 @@
+
+ model.select_ground_units_on_screen_1 = input.doubleTap(function () {
+    api.select.onScreenWithTypeFilter(model.currentFocusPlanetId(), 'Tank', 'Bot');
+  }, function () {
+    api.select.onPlanetWithTypeFilter(model.currentFocusPlanetId(), 'Tank', 'Bot');
+  })
+  
+  model.select_ground_units_on_screen_2 = input.doubleTap(function () {
+    api.select.onScreenWithTypeFilter(model.currentFocusPlanetId(), 'Tank', 'Bot');
+  }, function () {
+    api.select.onPlanetWithTypeFilter(model.currentFocusPlanetId(), 'Tank', 'Bot');
+  })
+  
+  model.select_ground_units_on_screen_3 = input.doubleTap(function () {
+    api.select.onScreenWithTypeFilter(model.currentFocusPlanetId(), 'Tank', 'Bot','Ground');
+  }, function () {
+    api.select.onPlanetWithTypeFilter(model.currentFocusPlanetId(), 'Tank', 'Bot','Ground');
+  })
+  
+  api.Panel.message('', 'inputmap.reload');
+	
